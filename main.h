5b0c2ee46a2b64feb6100d7b55c3d528668a053a
Mauriciourrozz <9366@holbertonstudents.com>
2024-08-14 21:30:23 +0000

    add main.h and simple shell

---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef simple_shell
+#define simple_shell
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#endif
---
+++ simple_shell.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	while (1)
+	{
+		printf("$ ");
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			free(line);
+			return (-1);
+		}
+	}
+		printf("%s", line);
+		free(line);
+		return (0);
+}
