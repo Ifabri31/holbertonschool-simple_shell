dfbc6e9462f24d2dd647217606cf3f9d25da6385
Fabri <oviedofabrizzio1998@gmail.com>
2024-08-19 16:13:47 +0000

    fix main, add token and include path.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,17 +1,55 @@
 #include "main.h"
-/**
- *
- */
+
+#define DELIM " \t\n"
+
+char count_tok(char *line)
+{
+	int count = 0;
+	char *token;
+
+	token = strtok(line, DELIM);
+	while (token)
+	{
+		count++;
+		token = strtok(NULL, DELIM);
+	}
+	return(count);
+}
+
+char **tokenizar(char *command)
+{
+	int i = 0;
+	int num_tok;
+	char *tok;
+	char **args;
+	char *copy;
+
+	copy = strdup(command);
+	num_tok = count_tok(command);
+	args = malloc((num_tok + 1) * sizeof(char*));
+	if (!args)
+	{
+		free(args);
+		return (NULL);
+	}
+	tok = strtok(copy, DELIM);
+	while (tok != NULL)
+	{
+		args[i] = strdup(tok);
+		tok = strtok(NULL, DELIM);
+		i++;
+	}
+	args[i] = NULL;
+	return(args);
+}
 
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	char *pathname;
 	char **argv;
 	pid_t child;
-	int status;
 	int _isatty = isatty(STDIN_FILENO);
 
 	while (1)
@@ -19,25 +57,38 @@ int main(void)
 		if (_isatty)
 			printf("$ ");
 		read = getline(&line, &len, stdin);
-		argv = token(line);
-		pathname = argv[0];
-		child = fork();
-		if (child == -1)
-                {
-			break;
-                }
 		if (read == -1)
 		{
+			return(-1);
 			break;
 		}
-		if (child == 0)
-			execve(pathname, argv, NULL);
+		argv = tokenizar(line);
+		if (argv == NULL)
+		{
+			free(argv);
+			break;
+		}
+		child = fork();
+		if (child < 0)
+                {
+			free(argv);
+			free(line);
+			return(-1);
+                }
+		else if (child > 0)
+			wait(&child);
 		else
-			wait(&status);
+		{
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				printf("./hsh: No such file or directory\n");
+				return (1);
+			}
+		}
 
+			wait(&child);
+		free(argv);
 	}
 	free(line);
-	return (0);
+	return(0);
 }
-
-
