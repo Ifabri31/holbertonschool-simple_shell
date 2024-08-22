794ddd97c52d789dec1bf19af940ac25c8bede88
Mauriciourrozz <9366@holbertonstudents.com>
2024-08-22 17:16:55 +0000

    update main.h

--- main.h
+++ main.h
@@ -6,4 +6,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 char *command_in_path(char *command);
+char *_getenv(char *data);
+char count_tok(char *line);
+char **tokenizar(char *command);
+int command_in_shell(char *line, char **envp);
 #endif
